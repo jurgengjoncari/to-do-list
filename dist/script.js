4051f703f1177487b1a3500a9089518a11b04deb
Jurgen Gjonçari <jurgen.gjoncari@gmail.com>
2022-04-20 07:03:19 +0200

    Improve styling

--- dist/script.js
+++ dist/script.js
@@ -14,10 +14,6 @@ button.addEventListener('click', () => {
     textInput.value = ''
 
     item.addEventListener('click', () => {
-        if (item.style.textDecoration == 'line-through') {
-            item.style.textDecoration = 'none'
-        } else {
-            item.style.textDecoration = 'line-through'
-        }
+        item.classList.toggle('checked')
     })
 })
