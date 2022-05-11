86c90f9a5ae5218788dfa96581a717a2f9d8606e
Jurgen Gjonçari <jurgen.gjoncari@gmail.com>
2022-05-11 22:32:43 +0200

    hi

--- dist/script.js
+++ dist/script.js
@@ -6,13 +6,12 @@ let submitButton = document.querySelector('#submit')
 
 function createItem(todo) {
     const listItem = document.createElement('li')
-    listItem.classList.add('list-group-item', 'd-flex', 'justify-content-between')
 
     listItem.innerHTML = `
         <label>
-            <input type="checkbox">
+            <input type="checkbox" class="form-check-input me-1">
         </label>
-        <button class="delete"></button>
+        <button class="delete btn btn-outline-danger"></button>
     `;
 
     listItem.querySelector("label").insertAdjacentText('beforeend', todo)
