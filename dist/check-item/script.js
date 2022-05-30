6e25f9defc7098c264ace13a41214beb2f175593
Jurgen Gjonçari <j.gjoncari@albansi.com>
2022-05-30 15:32:58 +0200

    Separate methods

--- dist/check-item/script.js
+++ dist/check-item/script.js
@@ -2,25 +2,35 @@ export default class CheckItem extends HTMLElement {
     constructor() {
         // @ts-ignore
         super().attachShadow({mode: 'open'})
-        const itemTemplate = document.querySelector('#check-item')
+        const itemTemplate = document.querySelector('template[data-check-item]')
         // @ts-ignore
         this.shadowRoot.append(itemTemplate.content.cloneNode(true));
 
-        // Update the HTML template and add events
-        const checkBox = this.shadowRoot.querySelector(`[type=checkbox]`)
-        const label = this.shadowRoot.querySelector("label")
-        const deleteButton = this.shadowRoot.querySelector("[data-delete]")
+        // Register properties
+        this.checkBox = this.shadowRoot.querySelector(`[type=checkbox]`)
+        this.label = this.shadowRoot.querySelector("label")
+        this.deleteButton = this.shadowRoot.querySelector("[data-delete]")
 
-        checkBox.addEventListener("click", () => label.classList.toggle("checked"))
-        deleteButton.addEventListener("click", () => this.remove())
+        this.setID()
 
-        // Insert Ids to checkbox and label
+        // Make it interactive
+        this.checkBox.addEventListener("click", () => this.handleClick())
+        this.deleteButton.addEventListener("click", () => this.handleDelete())
+    }
+
+    setID() {
         const id = `id_${Date.now()}`
 
-        checkBox.id = id
-        label.setAttribute('for', id)
+        this.checkBox.id = id
+        this.label.setAttribute('for', id)
+    }
 
-        // Store description in a span with slot
+    handleClick() {
+        this.label.classList.toggle("checked")
+    }
+
+    handleDelete() {
+        this.remove()
     }
 
     setDescription(description) {
